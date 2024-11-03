dec5b1154e6a6ede2e9a19642c0c4f5bfc675139
Kaan Kuscu <me@kaanksc.com>
2024-11-03 19:23:18 +0300

    add custom error types

--- errors.go
+++ errors.go
@@ -1,6 +1,9 @@
 package keyfile
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 var (
 	// Parameter
@@ -8,12 +11,63 @@ var (
 	ErrParameterMustNotBeNil  = errors.New("keyfile: parameter must not be nil")
 	ErrInvalidParameterType   = errors.New("keyfile: invalid parameter type")
 
-	// Section
-	ErrSectionKeyMustBeString = errors.New("keyfile: section key must be a string")
-
 	// Key-Value Pair
-	ErrKeyValuePairMustBeInSection   = errors.New("keyfile: key-value pair must be in a section")
-	ErrKeyValuePairMustBeStructOrMap = errors.New("keyfile: key-value pair must be a struct or map")
-	ErrKeyTypeMustBeString           = errors.New("keyfile: key type must be a string")
-	ErrUnsupportedValueType          = errors.New("keyfile: unsupported value type")
+	ErrUnsupportedValueType = errors.New("keyfile: unsupported value type")
+	ErrInvalidMapKeyType    = errors.New("invalid map key type, must be a string")
 )
+
+type ErrInvalidGroupName struct {
+	Line       string
+	LineNumber int
+}
+
+func (e ErrInvalidGroupName) Error() string {
+	return fmt.Sprintf("keyfile: line[%d] -> invalid group name: %q", e.LineNumber, e.Line)
+}
+
+type ErrKeyValuePairMustBeContainedInAGroup struct {
+	Line       string
+	LineNumber int
+}
+
+func (e ErrKeyValuePairMustBeContainedInAGroup) Error() string {
+	return fmt.Sprintf("keyfile: line[%d] -> key-value pair must be contained in a group: %q", e.LineNumber, e.Line)
+}
+
+type ErrInvalidEntry struct {
+	Line       string
+	LineNumber int
+}
+
+func (e ErrInvalidEntry) Error() string {
+	return fmt.Sprintf("keyfile: line[%d] -> invalid entry: %q", e.LineNumber, e.Line)
+}
+
+type ErrInvalidKey struct {
+	Line       string
+	LineNumber int
+}
+
+func (e ErrInvalidKey) Error() string {
+	return fmt.Sprintf("keyfile: line[%d] -> invalid key: %q", e.LineNumber, e.Line)
+}
+
+type ErrInvalidGroupType struct {
+	GroupName string
+	GroupType string
+}
+
+func (e ErrInvalidGroupType) Error() string {
+	return fmt.Sprintf("keyfile: invalid group type: %s %s", e.GroupName, e.GroupType)
+}
+
+type ErrCanNotParsed struct {
+	Err        error
+	SourceKey  string
+	TargetName string
+	TargetType string
+}
+
+func (e ErrCanNotParsed) Error() string {
+	return fmt.Sprintf("keyfile: can not parsed: from %q to \"%s %s\": %s", e.SourceKey, e.TargetName, e.TargetType, e.Err)
+}
